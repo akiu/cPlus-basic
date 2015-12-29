ad793021421e3970303810bd8f6f417bf05d6492
ricky <ricky3andika@gmail.com>
2015-12-29 19:17:18 +0700

    second

---
+++ enum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	enum nama {budi, maman, amam} x;
+	x = amam;
+	cout << x;
+	return 0;
+}
---
+++ var.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char a;
+	
+	a = 1;
+
+	cout << a;
+
+	return 0;
+}
